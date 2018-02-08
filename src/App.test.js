fb5ebf29c298570ac6ab1ee293bafcb9a1b0b7ea
William Mosley <mosleywm@gmail.com>
2018-02-07 17:40:49 -0800

    Add GiftDetail, Footer and App component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('should render without crashing', () => {
+  shallow(<App />);
 });
---
+++ src/components/GiftDetail.spec.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {createShallow, createMount} from 'material-ui/test-utils';
+import GiftDetail from './GiftDetail';
+
+describe('(Component) GiftDetail', () => {
+  let shallow;
+  let mount;
+
+  const GIFT = {
+    name: 'Test Name',
+    priority: true,
+    description: 'Test Description'
+  };
+  const INDEX = 0;
+
+  const handleCheck = jest.fn();
+  const handleInputChange = jest.fn();
+  const handleRemoveItem = jest.fn();
+
+  const component = <GiftDetail
+    gift={GIFT}
+    index={INDEX}
+    handleCheck={handleCheck}
+    handleInputChange={handleInputChange}
+    handleRemoveItem={handleRemoveItem}
+  />;
+
+  beforeEach(() => {
+    shallow = createShallow({dive: true});
+    mount = createMount();
+  });
+
+  it('should render without crashing', () => {
+    shallow(component);
+  });
+
+  it('should render gift properties', () => {
+    const wrapper = shallow(component);
+    expect(wrapper.find('[name="name"]')).toHaveValue(GIFT.name);
+    expect(wrapper.find('[name="description"]')).toHaveValue(GIFT.description);
+  });
+
+  it('should call update functions', () => {
+    const wrapper = shallow(component);
+    wrapper.find('[name="name"]').simulate('change');
+    wrapper.find('[name="description"]').simulate('change');
+    expect(handleInputChange).toHaveBeenCalledTimes(2);
+    expect(handleInputChange).toHaveBeenCalledWith(INDEX, undefined);
+  });
+
+  it('should render gift priority', () => {
+    const wrapper = mount(component);
+    expect(wrapper.find('input [name="priority"]')).toBeChecked();
+  });
+
+  it('should call update checkbox functions', () => {
+    const wrapper = mount(component);
+    wrapper.find('input [name="priority"]').simulate('change');
+    expect(handleCheck).toHaveBeenCalledWith(INDEX, expect.anything());
+  });
+
+  it('should call remove function', () => {
+    const wrapper = mount(component);
+    wrapper.find('button').simulate('click');
+    expect(handleRemoveItem).toHaveBeenCalledWith(INDEX);
+  });
+});
